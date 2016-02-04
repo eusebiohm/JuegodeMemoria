4cff14b9af2fa5b3864ebc2e21fa458bd274e381
Eusebio Hernandez Mares <eusebiohernandezmares@macbook-air-de-eusebio.local>
2016-02-04 10:51:49 -0600

    esta es el comité  de juego de memoria

---
+++ JuegodeMemoria.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numeros = 0...100
+
+for n in numeros {
+    if n == 0 {print("")}
+        
+    else
+        if  ((n > 29) && (n < 41)){
+            print("\(n) Viva Swift")
+        }else {
+            if (n % 5 == 0) {
+                
+                print("\(n)  Bingo !!!")}
+            else {
+                if (n % 2 == 0) {
+                    
+                    print("\(n)  par !!!")}
+                else {
+                    if (n % 1 == 0) {
+                        
+                        print("\(n)  impar !!!")}
+                    
+                }
+            }      }
+}
